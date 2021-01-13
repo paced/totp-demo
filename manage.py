ac4fd1b4a0cbd5358d3d0689a856b5efad75625d
Thomas Wang <thomas@teaminkling.com>
2021-01-13 14:08:48 +1100

    Minor cosmetic code changes

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ from flask import redirect, render_template, request, send_from_directory
 from utils import launchServer
 
 # Necessary settings
+
 DEBUG = False
 
 if DEBUG:
@@ -27,42 +28,49 @@ app = launchServer(DEBUG, CSSFILES, JSFILES)
 @app.context_processor
 def inject_debug():
     """Inject debug state into every template."""
+
     return dict(debug=app.debug)
 
 
 @app.errorhandler(404)
 def notFound(e):
     """View for 404 page."""
+
     return render_template('content/notfound.jinja.html'), 404
 
 
 @app.route('/', methods=['GET'])
 def index():
     """Home page with basic TOTP explanation."""
+
     return render_template('content/index.jinja.html')
 
 
 @app.route('/security/', methods=['GET'])
 def security():
     """Security explanation page."""
+
     return render_template('content/security.jinja.html')
 
 
 @app.route('/demo/', methods=['GET'])
 def demo():
     """Demo page."""
+
     return render_template('content/demo.jinja.html')
 
 
 @app.route('/api/status', methods=['GET'])
 def status():
     """Simple API response to see if API is alive."""
+
     return "OK"
 
 
 @app.route('/api/endpoint', methods=['GET'])
 def endpoint():
     """API endpoint to generate a TOTP token."""
+
     try:
         secret = request.args["key"]
         interval = int(request.args["interval"])
@@ -91,22 +99,28 @@ def endpoint():
 @app.route('/sitemap.xml', methods=['GET'])
 def sitemap():
     """Contact forms."""
+
     return send_from_directory('static/', 'sitemap.xml')
 
 
 @app.route('/favicon.ico')
 def favicon():
     """Favicon for IE-based browsers."""
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicons/favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
+
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicons/favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 
 @app.route('/robots.txt')
 def robots():
     """Robot file for SEO."""
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'robots.txt')
+
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'), 'robots.txt'
+    )
 
 
 if __name__ == "__main__":
